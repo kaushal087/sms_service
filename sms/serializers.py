bbf4c33c57766d02072fa200061e98c5411f8171
Kaushal <kaushal499@gmail.com>
2018-08-22 15:46:54 +0530

    custom error validation response

--- sms/serializers.py
+++ sms/serializers.py
@@ -1,29 +1,69 @@
 from rest_framework import serializers
 
+
 class InboundSMSSerializer(serializers.Serializer):
 
-    from_ = serializers.CharField(required=True, min_length=6, max_length=16)
-    to = serializers.CharField(required=True, min_length=6, max_length=16)
-    text = serializers.CharField(required=True, min_length=1, max_length=120)
-
+    from_ = serializers.CharField()
+    to = serializers.CharField()
+    text = serializers.CharField()
 
     def __init__(self, *args, **kwargs):
         super(InboundSMSSerializer, self).__init__(*args, **kwargs)
         self.fields['from'] = self.fields['from_']
         del self.fields['from_']
+        self.fields['from'].error_messages[
+            'required'] = 'from is missing'
+        self.fields['to'].error_messages[
+            'required'] = 'to is missing'
+        self.fields['text'].error_messages[
+            'required'] = 'text is missing'
 
-        # self.fields['from'].error_messages['required'] = 'from field is required'
-        # self.fields['from'].error_messages['min_length'] = 'Minimum 6 length is required'
 
+    def validate_to(self, to, *args, **kwargs):
+        if not to:
+            raise serializers.ValidationError("to is missing")
+        if len(to) < 6 or len(to) > 16:
+            raise serializers.ValidationError("to is invalid")
+
+    def validate_from(self, from_, *args, **kwargs):
+        if not from_:
+            raise serializers.ValidationError("from is missing")
+        if len(from_) < 6 or len(from_) > 16:
+            raise serializers.ValidationError("from is invalid")
+
+    def validate_text(self, text, *args, **kwargs):
+        if not text:
+            raise serializers.ValidationError("text is missing")
+        if len(text) < 1 or len(text) > 120:
+            raise serializers.ValidationError("text is invalid")
 
 
 class OutboundSMSSerializer(serializers.Serializer):
 
-    from_ = serializers.CharField(required=True, min_length=6, max_length=16)
-    to = serializers.CharField(required=True, min_length=6, max_length=16)
-    text = serializers.CharField(required=True, min_length=1, max_length=120)
+    from_ = serializers.CharField()
+    to = serializers.CharField()
+    text = serializers.CharField()
 
     def __init__(self, *args, **kwargs):
         super(OutboundSMSSerializer, self).__init__(*args, **kwargs)
         self.fields['from'] = self.fields['from_']
         del self.fields['from_']
+        self.fields['from'].error_messages[
+            'required'] = 'from is missing'
+        self.fields['to'].error_messages[
+            'required'] = 'to is missing'
+        self.fields['text'].error_messages[
+            'required'] = 'text is missing'
+
+    def validate_to(self, to, *args, **kwargs):
+        if len(to) < 6 or len(to) > 16:
+            raise serializers.ValidationError("to is invalid")
+
+    def validate_from(self, from_, *args, **kwargs):
+        if len(from_) < 6 or len(from_) > 16:
+            raise serializers.ValidationError("from is invalid")
+
+    def validate_text(self, text, *args, **kwargs):
+        if len(text) < 1 or len(text) > 120:
+            raise serializers.ValidationError("text is invalid")
+
