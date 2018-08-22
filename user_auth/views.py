a74f474a9fe41caa149967c5092567398aa72aad
Kaushal <kaushal499@gmail.com>
2018-08-23 03:16:01 +0530

    update

--- user_auth/views.py
+++ user_auth/views.py
@@ -53,8 +53,8 @@ class SignUp(APIView):
                  'error': 'User with given information already exist'},
                 status=HTTP_400_BAD_REQUEST)
 
-        user = User.objects.create(username='test_user1', email=email)
-        user.set_password('12345')
+        user = User.objects.create(username=username, email=email)
+        user.set_password(password)
         user.save()
 
         return Response({'message':{'user_id': user.id, 'success': True}, 'error':''})
