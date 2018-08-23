feaf6ac4f37980a47da8ab6067b10183875b9f54
Kaushal <kaushal499@gmail.com>
2018-08-23 14:29:58 +0530

    secret key in environment variable

--- sms_service/settings.py
+++ sms_service/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'rpxwyx#!9c3d36s^=-4-at^*(w2r4ea-zwr*+15u+1hpju*lp@'
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
